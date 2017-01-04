325f06e7151a84fc617c8d60ec172447ca3282fd
Isak Sondell <isak.sondell@gmail.com>
2017-01-04 13:18:23 +0100

    init

---
+++ Assets/Scripts/LevelInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    class LevelInput : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                SceneManager.LoadScene("Attractors");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Objects.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Objects : MonoBehaviour
+{
+    [SerializeField]
+    private List<Rigidbody> m_rigidBodies;
+	
+    void Update()
+    {
+        foreach (Rigidbody body in m_rigidBodies)
+        {
+            for (int i = 0; i < m_rigidBodies.Count; i++)
+            {
+                if (m_rigidBodies[i] != body)
+                {
+                    Gravitate(m_rigidBodies[i], body);
+                }
+            }
+        }
+    }
+
+
+    private void Gravitate(Rigidbody subject, Rigidbody source)
+    {
+        
+        Vector3 gravity = new Vector3();
+        float gravityOverDistance = source.mass / Vector3.SqrMagnitude(source.position - subject.position);
+        gravity += (source.position - subject.position).normalized * gravityOverDistance;
+
+        subject.AddForce(gravity, ForceMode.Acceleration);        
+    }
+}
+
